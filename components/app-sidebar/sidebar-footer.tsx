6d16bcbde29f201955d5ae5c7eb8b63e132a572e
Asaad Ahmed <asaadbinahmed@gmail.com>
2026-02-06 23:57:46 -0500

    feat: add import and export buttons to sidebar footer

--- components/app-sidebar/sidebar-footer.tsx
+++ components/app-sidebar/sidebar-footer.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { SidebarFooter } from "@/components/ui/sidebar";
 import { ModeToggle } from "@/components/mode-toggle";
-import { Plus } from "lucide-react";
+import { Plus, Import, Upload } from "lucide-react";
 
 type SidebarFooterProps = {
   onAddCollection: () => void;
@@ -13,12 +13,23 @@ export default function AppSidebarFooter({
   onAddCollection,
 }: SidebarFooterProps) {
   return (
-    <SidebarFooter className="mb-2 flex w-full flex-row gap-2">
-      <Button variant="outline" className="flex-1" onClick={onAddCollection}>
-        <Plus className="size-4" />
-        Add Collection
-      </Button>
-      <ModeToggle />
+    <SidebarFooter className="mb-2 flex w-full flex-col gap-2">
+      <div className="flex flex-row justify-between gap-2">
+        <Button variant="outline" className="flex-1" onClick={onAddCollection}>
+          <Plus className="size-4" />
+          Add Collection
+        </Button>
+        <ModeToggle />
+      </div>
+      <div className="flex flex-row justify-between gap-2">
+        <Button variant="outline" className="flex-1">
+          <Import className="size-4" /> Import
+        </Button>
+        <Button variant="outline" className="flex-1">
+          <Upload className="size-4" />
+          Export
+        </Button>
+      </div>
     </SidebarFooter>
   );
 }
